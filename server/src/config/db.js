6346031ac8a6066cc96edea6c4cb6063250e67cc
Prathamesh <prathmeshdhadbale@gmail.com>
2026-04-15 14:46:50 +0530

    db change

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,21 +4,15 @@ dotenv.config();
 import pg from "pg";
 const { Pool } = pg;
 
-
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 pool.connect()
-    .then(() => {
-        console.log("Connected to database");
-    })
-    .catch((err) => {
-        console.error("Error connecting to database:", err.message);
-    });
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.error("Error connecting to database:", err.message));
 
 export default pool;
